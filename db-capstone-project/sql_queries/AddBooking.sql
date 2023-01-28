c6b007925400b53d067190c0e90e29730bbc9afb
WindAlan-sw <76271974+WindAlan-sw@users.noreply.github.com>
2023-01-28 01:41:58 +0000

    Create AddBooking.sql

---
+++ db-capstone-project/sql_queries/AddBooking.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+CREATE PROCEDURE AddBooking(BookingIDInput INT, CustomerIDInput INT, 
+BookingDateInput DATETIME, TableNoInput INT)
+BEGIN
+	DECLARE result VARCHAR(255);
+    DECLARE n_bookings INT;
+    
+    SELECT COUNT(*) INTO n_bookings
+    FROM Bookings
+    WHERE BookingDate = BookingDateInput AND TableNo=TableNoInput;
+    
+    IF n_bookings = 0 THEN
+		SET result="New booking added.";
+	ELSE SET result="The table is booked that time.";
+	END IF;
+    IF n_bookings = 0 THEN
+		INSERT INTO Bookings(BookingID, BookingDate, TableNo, CustomerID)
+		VALUES(BookingIDInput,BookingDateInput, TableNoInput, CustomerIDInput);
+	END IF;
+	SELECT result;
+END
